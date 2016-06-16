35363473b436e2a8c7e7982e1377d5f25ffaec1c
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2016-06-16 15:09:16 +0900

    Refactoring: Rename and format

--- lib/rafted_value.ex
+++ lib/rafted_value.ex
@@ -5,20 +5,20 @@ defmodule RaftedValue do
 
   @type consensus_group_info :: {:create_new_consensus_group, Config.t} | {:join_existing_consensus_group, [ServerId.t]}
 
-  defun start_link(consensus_group_info :: consensus_group_info, name_or_nil :: g[atom] \\ nil) :: GenServer.on_start do
-    case consensus_group_info do
+  defun start_link(info :: consensus_group_info, name_or_nil :: g[atom] \\ nil) :: GenServer.on_start do
+    case info do
       {:create_new_consensus_group   , %Config{}    }                             -> :ok
       {:join_existing_consensus_group, known_members} when is_list(known_members) -> :ok
       # raise otherwise
     end
-    start_link_impl(consensus_group_info, name_or_nil)
+    start_link_impl(info, name_or_nil)
   end
 
-  defp start_link_impl(arg, name) do
+  defp start_link_impl(info, name) do
     if name do
-      :gen_fsm.start_link({:local, name}, Server, arg, [])
+      :gen_fsm.start_link({:local, name}, Server, info, [])
     else
-      :gen_fsm.start_link(Server, arg, [])
+      :gen_fsm.start_link(Server, info, [])
     end
   end
 
@@ -36,7 +36,10 @@ defmodule RaftedValue do
 
   @type command_identifier :: reference | any
 
-  defun run_command(leader :: GenServer.server, command_arg :: any, timeout :: timeout \\ 5000, id :: command_identifier \\ make_ref) :: {:ok, any} | {:error, atom} do
+  defun run_command(leader      :: GenServer.server,
+                    command_arg :: any,
+                    timeout     :: timeout \\ 5000,
+                    id          :: command_identifier \\ make_ref) :: {:ok, any} | {:error, atom} do
     try do
       :gen_fsm.sync_send_event(leader, {:command, command_arg, id}, timeout)
     catch
