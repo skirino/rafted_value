335caa55756ea8c83968d39dbb22b81e0f429b4f
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2017-08-22 20:02:31 +0900

    v0.6.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule RaftedValue.Mixfile do
   def project() do
     [
       app:             :rafted_value,
-      version:         "0.5.0",
+      version:         "0.6.0",
       elixir:          "~> 1.4",
       build_embedded:  Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
