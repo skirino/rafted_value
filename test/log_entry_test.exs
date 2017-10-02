1046917abbeb12bcfb3442d8201217cffb841213
Kyle <hanooter@gmail.com>
2017-10-02 16:17:31 -0700

    Remove :change_config message

--- test/log_entry_test.exs
+++ test/log_entry_test.exs
@@ -20,7 +20,6 @@ defmodule RaftedValue.LogEntryTest do
     [
       {1, 2, :command           , {make_gen_server_from(), :some_command_arg, make_ref()}},
       {1, 2, :query             , {make_gen_server_from(), :some_query_arg}},
-      {1, 2, :change_config     , RaftedValue.make_config(__MODULE__)},
       {1, 2, :leader_elected    , self()},
       {1, 2, :add_follower      , self()},
       {1, 2, :remove_follower   , self()},
