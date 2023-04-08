ee3db01c81ffbd077e2d1f4e93dc81156bf634c1
Shunsuke Kirino <skirino@flywheel.jp>
2023-04-09 02:50:12 +0900

    Update dev/test-only deps

--- mix.exs
+++ mix.exs
@@ -27,9 +27,9 @@ defmodule RaftedValue.Mixfile do
   defp deps() do
     [
       {:croma      , "~> 0.9"},
-      {:dialyxir   , "~> 0.5" , [only: :dev , runtime: false]},
-      {:ex_doc     , "~> 0.20", [only: :dev , runtime: false]},
-      {:excoveralls, "~> 0.11", [only: :test, runtime: false]},
+      {:dialyxir   , "~> 1.3" , [only: :dev , runtime: false]},
+      {:ex_doc     , "~> 0.29", [only: :dev , runtime: false]},
+      {:excoveralls, "~> 0.16", [only: :test, runtime: false]},
     ]
   end
 
